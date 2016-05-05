2036e1eb550053d53702eb9c2b10de2f75b0a2b5
Александр Прендота <aleksandrprendota@MacBook-Air-Aleksandr.local>
2016-05-05 20:25:23 +0300

    add changing image and prepare for adding design

---
+++ Backet/FinalViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FinalViewController.swift
+//  Backet
+//
+//  Created by Александр Прендота on 05.05.16.
+//  Copyright © 2016 Александр Прендота. All rights reserved.
+//
+
+import UIKit
+
+class FinalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
--- Backet/SecondViewController.swift
+++ Backet/SecondViewController.swift
@@ -12,24 +12,29 @@ class SecondViewController: UIViewController {
     
 
     var countAll :Int = 0
-    
+
+
     @IBOutlet weak var imageMain: UIImageView!
-    @IBOutlet weak var countLabel: UILabel!
-    @IBAction func returnButton(sender: UIButton) {
+    @IBAction func finalButton(sender: UIButton) {
+         self.performSegueWithIdentifier("getFinal", sender: sender)
     }
-   
-   
+    @IBOutlet weak var countLabel: UILabel!
+    
+    @IBOutlet weak var finalBut: UIButton!
     override func viewDidLoad() {
+       
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        finalBut.hidden = true
         countAll = NSUserDefaults.standardUserDefaults().integerForKey("launchCount")
         
         if countAll == 0 {
-            countAll = 10
+            countAll = 100
         }
         
         countLabel.text = "\(countAll)"
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -39,16 +44,31 @@ class SecondViewController: UIViewController {
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         
+        if countAll == 750 {
+            countAll -= 1
+            self.imageMain.image = UIImage(named: "nope.jpg")
+        } else if countAll == 745 {
+            finalBut.hidden = false
+            countLabel.text = "вин"
+        } else {
         countAll -= 1
         countLabel.text = "\(countAll)"
+            
+        let bounds = imageMain.bounds
+        UIView.animateWithDuration(1.0, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10, options:[], animations: {
+            self.imageMain.bounds = CGRect(x: bounds.origin.x - 10, y: bounds.origin.y, width: bounds.size.width + 50,height: bounds.size.height)
+            }, completion: nil)
+        }
+     
+      
         let userDefaults = NSUserDefaults.standardUserDefaults()
         userDefaults.setInteger(countAll, forKey: "launchCount")
         userDefaults.synchronize()
         
-        let bounds = imageMain.bounds
-        UIView.animateWithDuration(1.0, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10, options:[], animations: {
-        self.imageMain.bounds = CGRect(x: bounds.origin.x - 10, y: bounds.origin.y, width: bounds.size.width + 50,height: bounds.size.height)
-        }, completion: nil)
-        
+
+    }
+
+    @IBAction func returnButton(sender: UIButton) {
+     
     }
 }
